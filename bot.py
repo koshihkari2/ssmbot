eda0ce6ceaf36b121a5acc425436a151f695bd07
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2020-01-01 20:42:22 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,16 @@ import traceback
 EXT = ["cogs.recruit"]
 token = os.environ.get("TOKEN","")
 
+class JapaneseHelpCommand(commands.DefaultHelpCommand):
+    def __init__(self):
+        super().__init__()
+        self.commands_heading = "コマンド:"
+        self.no_category = "未分類"
+        self.command_attrs["help"] = "ヘルプコマンド"
+
+    def get_ending_note(self):
+        return ("各コマンドの説明: _help コマンド名\n各カテゴリの説明: _help カテゴリ名\n")
+
 class DiscordBot(commands.Bot):
     def __init__(self,command_prefix):
         super().__init__(command_prefix)
@@ -19,10 +29,12 @@ class DiscordBot(commands.Bot):
                 traceback.print_exc()
 
     async def on_message(self,message):
+        if self.user in message.mentions:
+            await message.channel.send("`_help` でコマンド一覧が確認できます。")
         await self.process_commands(message)
         
     async def on_ready(self):
         print("ready")
         
-bot = DiscordBot("_")
+bot = DiscordBot("_",help_command=JapaneseHelpCommand())
 bot.run(token)
