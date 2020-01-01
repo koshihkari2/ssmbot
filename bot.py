a551e388f7064bbaada1b7f9f366d2ceabf995c6
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2020-01-01 13:08:44 +0900

    create

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+import os
+
+EXT = ["cogs.recruit"]
+token = os.environ.get("TOKEN","")
+
+class DiscordBot(commands.Bot):
+    def __init__(self,command_prefix):
+        super().__init__(command_prefix)
+
+        for cog in EXT:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
+
+    async def on_message(self,message):
+        self.process_commands(message)
+        
+    async def on_ready(self):
+        print("ready")
+        
+bot = DiscordBot("_")
+bot.run(token)
