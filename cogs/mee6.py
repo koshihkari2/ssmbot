c40e16220530d07afc53a04b05ea4248122ad523
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2020-02-18 17:37:56 +0900

    Update mee6.py

--- cogs/mee6.py
+++ cogs/mee6.py
@@ -12,12 +12,12 @@ class MeeSix(commands.Cog,name="Mee6"):
         if message.content == "!levels" and mee6 in message.guild.members:
             api = API(message.guild.id)
             result = await api.levels.get_leaderboard_page(0)
-            embed = discord.Embed(title=f"{message.guild.name} 詳細順位表 (Top25)",color=0x00ffff)
+            embed = discord.Embed(title=f"{message.guild.name} 詳細順位表 (Top10)",color=0x00ffff)
             
             players = result["players"][:10]
             for i,player in enumerate(players):
                 tmp = f"レベル：{player['level']}\nメッセージ数：{player['message_count']}\n経験値：{player['xp']}"
-                embed.add_field(name=f"{player['username']} ({i + 1} 位)",value=tmp,inline=bool(i%2))
+                embed.add_field(name=f"{player['username']} ({i + 1} 位)",value=tmp,inline=i%3)
                 
             await message.channel.send(embed=embed)
 
