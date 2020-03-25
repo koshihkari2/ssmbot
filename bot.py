8850800ae0a2d2383d7454dab9a6ecb20d827aa2
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2020-03-25 13:35:51 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 import traceback
 
 
-EXT = ["cogs.recruit","cogs.sym","cogs.spla","cogs.open_link","cogs.mee6","cogs.ssm","cogs.voice"]
+EXT = ["cogs.recruit","cogs.sym","cogs.spla","cogs.open_link","cogs.mee6","cogs.ssm","cogs.voice","cogs.search"]
 token = os.environ.get("TOKEN","")
 
 class JapaneseHelpCommand(commands.DefaultHelpCommand):
