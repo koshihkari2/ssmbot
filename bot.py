11bc6168fdd1e13f290b68094feffef7c7227492
coolwind0202 <51913600+coolwind0202@users.noreply.github.com>
2020-04-06 11:01:20 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,15 @@ import os
 import traceback
 import glob
 
-files = glob.glob("/cogs/*")
+import os
+path = ''
+for d, s, f in os.walk(path):
+    print('')
+    print('── {}'.format(d))
+    print(' └── {}'.format(s))
+    print('   └── {}'.format(f))
+
+files = glob.glob("cogs/*")
 EXT = [f"{cogs.file_name}" for file_name in files]
 token = os.environ.get("TOKEN","")
 
